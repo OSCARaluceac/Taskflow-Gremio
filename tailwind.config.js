bcde5592dc62f092bfd29d81736f6bd7bf6166a5
OSCARaluceac <oscar.martin@alu.ceacfp.es>
2026-03-12 20:25:38 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        wood: '#5d4037',
+        gold: '#c5a028',
+        parchment: {
+          light: '#fdf6e3',
+          dark: '#121212',
+          DEFAULT: '#fafaf0'
+        }
+      },
+      fontFamily: {
+        pixel: ['"Press Start 2P"', 'cursive'],
+        serif: ['Lora', 'serif'],
+      }
+    }
+  }
+}
