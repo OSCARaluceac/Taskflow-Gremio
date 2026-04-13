00db382dd994991a404cca21ffb91e95f4e82983
Osk <oscar.martin@alu.ceacfp.es>
2026-04-13 02:40:24 +0200

    fix: eliminacion forzada y definitiva de YAML

--- server/src/routes/task.routes.js
+++ server/src/routes/task.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/task.controller');
 
-// Ya no hay YAML aquí. El editor no podrá sabotearnos.
 router.get('/', taskController.getTasks);
 router.post('/', taskController.createTask);
 router.patch('/:id', taskController.updateTaskStatus);
